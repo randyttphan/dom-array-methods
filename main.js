c5245f5682b86007d5e6965dab81b0e27b783dca
randyttphan <randyttphan@gmail.com>
2020-02-01 16:35:41 -0600

    function to format number to money

--- main.js
+++ main.js
@@ -40,9 +40,14 @@ function updateDOM(providedData = data) {
   providedData.forEach(item => {
     const element = document.createElement("div");
     element.classList.add("person");
-    element.innerHTML = `<strong>${item.name}</strong> ${item.money}`;
+    element.innerHTML = `<strong>${item.name}</strong> ${formatMoney(
+      item.money
+    )}`;
     main.appendChild(element);
   });
 }
 
-//Event Listeners
+// Format number as money - https://stackoverflow.com/questions/149055/how-to-format-numbers-as-currency-string
+function formatMoney(number) {
+  return "$" + number.toFixed(2).replace(/\d(?=(\d{3})+\.)/g, "$&,");
+}
